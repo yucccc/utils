cd2e4b7269a4e52ce9c792dfab14e012a33c78ca
yucccc <feyucccc@gmail.com>
2022-11-19 18:32:23 +0800

    fix: 去掉url默认参数

--- src/__tests__/url.test.ts
+++ src/__tests__/url.test.ts
@@ -1,5 +1,7 @@
 import { expect, test } from 'vitest'
 import { getURLParameters } from '../url'
+
 test('getURLParameters', () => {
     expect(getURLParameters('https://vitest.dev/?a=1&b=2')).toEqual({ a: '1', b: '2' })
+    expect(getURLParameters('')).toEqual({})
 })
--- src/url.ts
+++ src/url.ts
@@ -3,10 +3,13 @@
  * @param url url地址 默认值 window.location.href
  * @returns {}
  */
-export const getURLParameters = <T extends object>(url: string = window.location.href): T => (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
-    (a, v) => (
-        // @ts-ignore
-        (a[v.slice(0, v.indexOf("="))] = v.slice(v.indexOf("=") + 1)), a
-    ),
-    {} as T
-);
+export const getURLParameters = <T extends object>(url: string): T => {
+    if (!url) return {} as T
+    return (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
+        (a, v) => (
+            // @ts-ignore
+            (a[v.slice(0, v.indexOf("="))] = v.slice(v.indexOf("=") + 1)), a
+        ),
+        {} as T
+    );
+}
