9a9fb07a1b10d3b0b44332c7e564978f664904ca
yucccc <feyucccc@gmail.com>
2022-12-02 13:34:12 +0800

    fix: vite dependencies

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vitepress'
 
-import { props2table } from 'vitepress-plugin-props2table'
-
 export default defineConfig({
   title: 'Practical Utils',
   description: 'Practical business tools methodology package.',
